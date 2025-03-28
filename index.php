814c05e9c13b0f6f982d0d5a23fe99af49edf8a0
Christian <60179743+christian-2hu@users.noreply.github.com>
2025-03-28 20:00:57 -0300

    Remove unused object

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 require_once "vendor/autoload.php";
 require_once "config/Twig.php";
-require_once "config/Install.php";
 require_once "config/Database.php";
 require_once "config/Contents.php";
 
@@ -10,7 +9,6 @@ $dotenv->load();
 
 $database = new Database($_ENV['DB_DATABASE'], $_ENV['DB_USER'], $_ENV['DB_PASS']);
 $template = new Twig($_ENV['TWIG_PATH'], false);
-$install = new Install($database->getPdo());
 $contents = new Contents($database->getPdo());
 $twig = $template->getTwig();
 echo $twig->render('index.twig', [
